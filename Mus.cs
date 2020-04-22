11c7665ecdb8a95524f067db06c6b3294b6eed49
sinshu <sinshu.d@gmail.com>
2020-04-22 20:27:25 +0900

    Now the decoding process seems to run correctly.

--- Mus.cs
+++ Mus.cs
@@ -22,7 +22,7 @@ public class Mus
         var instruments = new int[instrCnt];
         for (var i = 0; i < instruments.Length; i++)
         {
-            instruments[i] = (int)BitConverter.ToUInt16(data, 14 + 2 * i);
+            instruments[i] = (int)BitConverter.ToUInt16(data, 16 + 2 * i);
         }
 
         Console.WriteLine("scoreLen = " + scoreLen);
@@ -34,6 +34,100 @@ public class Mus
         {
             Console.WriteLine("instruments[" + i + "] = " + instruments[i]);
         }
+
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+
+        var totalTime = 0;
+
+        var p = scoreStart;
+        while (true)
+        {
+            var channelNumber = data[p] & 0xF;
+            var eventType = (data[p] & 0x70) >> 4;
+            var last = (data[p] >> 7) != 0;
+
+            Console.WriteLine("channelNumber = " + channelNumber);
+            Console.WriteLine("eventType = " + eventType);
+            Console.WriteLine("last = " + last);
+
+            p++;
+
+            switch (eventType)
+            {
+                case 0:
+                    var releaseNote = data[p++];
+                    Console.WriteLine("    // Release note");
+                    Console.WriteLine("    releaseNote = " + releaseNote);
+                    break;
+                case 1:
+                    var playNote = data[p++];
+                    var noteNumber = playNote & 127;
+                    var noteVolume = (playNote & 128) != 0 ? data[p++] : -1;
+                    Console.WriteLine("    // Play note");
+                    Console.WriteLine("    playNote = " + playNote);
+                    if (noteVolume != -1)
+                    {
+                        Console.WriteLine("    noteVolume = " + noteVolume);
+                    }
+                    break;
+                case 2:
+                    var pitchWheel = data[p++];
+                    Console.WriteLine("    // Pitch wheel");
+                    Console.WriteLine("    pitchWheel = " + pitchWheel);
+                    break;
+                case 3:
+                    var systemEvent = data[p++];
+                    Console.WriteLine("    // System event");
+                    Console.WriteLine("    systemEvent = " + systemEvent);
+                    break;
+                case 4:
+                    var controllerNumber = data[p++];
+                    var controllerValue = data[p++];
+                    Console.WriteLine("    // Change controller");
+                    Console.WriteLine("    controllerNumber = " + controllerNumber);
+                    Console.WriteLine("    controllerValue = " + controllerValue);
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    Console.WriteLine("END!!!");
+                    goto end;
+                case 7:
+                    break;
+                default:
+                    throw new Exception("OOPS!!!");
+            }
+
+            Console.WriteLine();
+
+            if (last)
+            {
+                var time = 0;
+                while (true)
+                {
+                    var value = data[p++];
+                    time = time * 128 + value & 127;
+                    if ((value & 128) == 0)
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine("================================================================================");
+                Console.WriteLine(" TIME: " + time);
+                Console.WriteLine("================================================================================");
+                Console.WriteLine();
+
+                totalTime += time;
+            }
+        }
+
+    end:
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine("TOTAL TIME: " + totalTime + " (" + (totalTime / 140.0) + " sec)");
+        Console.WriteLine(p + " / " + data.Length);
     }
 
     private void CheckHeader(byte[] data)
